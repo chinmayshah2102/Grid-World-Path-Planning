f278f421047653d8a08a43ad7f199b9159e00510
Chinmay Shah <38117206+chinmayshah2102@users.noreply.github.com>
2019-05-22 16:15:36 -0400

    Implements purely random search planning

---
+++ RandomSearch.py
@@ -0,0 +1,39 @@
+import numpy as np
+from generateMapProperties import genGridProp
+import random as rd
+ 
+ 
+def Search(world_state, robot_pose, goal_pose):
+    
+    MapProperties = genGridProp(world_state)
+    barriers = MapProperties.barriers
+    
+    max_step_number = 200
+    
+    currPos = robot_pose
+    path = [robot_pose]
+
+    for i in range(max_step_number):
+        
+        #Checking if we have reached the goal
+        if currPos != goal_pose:
+            possibleNeighbours = MapProperties.NeighbourVertices(currPos, world_state, 4)
+            actualNeighbours = []
+            
+            for neighbour in possibleNeighbours:
+                if neighbour not in barriers:
+                    actualNeighbours.append(neighbour)
+            
+            index = rd.randrange(len(actualNeighbours))
+            newPos = actualNeighbours[index]
+                            
+            currPos = newPos
+            path.append(currPos)
+        
+        #If current position is goal position return the path
+        else:
+            return path
+    #if after maximum steps the current position is not the goal position then return no solution
+    if path[-1] != goal_pose:
+        return "No solution found"
+    
