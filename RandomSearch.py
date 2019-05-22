e53afee2ed904dd9101e7986953d3f1061af25f6
Chinmay Shah <38117206+chinmayshah2102@users.noreply.github.com>
2019-05-22 16:24:55 -0400

    Implements purely random search in grid world env

--- RandomSearch.py
+++ RandomSearch.py
@@ -1,9 +1,8 @@
-import numpy as np
 from generateMapProperties import genGridProp
 import random as rd
  
  
-def Search(world_state, robot_pose, goal_pose):
+def Search(world_state, robot_pose, goal_pose, number_of_actions):
     
     MapProperties = genGridProp(world_state)
     barriers = MapProperties.barriers
@@ -17,7 +16,7 @@ def Search(world_state, robot_pose, goal_pose):
         
         #Checking if we have reached the goal
         if currPos != goal_pose:
-            possibleNeighbours = MapProperties.NeighbourVertices(currPos, world_state, 4)
+            possibleNeighbours = MapProperties.NeighbourVertices(currPos, world_state, number_of_actions)
             actualNeighbours = []
             
             for neighbour in possibleNeighbours:
